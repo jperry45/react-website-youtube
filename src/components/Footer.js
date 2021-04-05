853921ff9fc1485bf8e87d422ee7b2a0224ea3ca
Joshua Perry <joshuacperry95@gmail.com>
2021-04-05 19:53:19 -0400

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <small class='website-rights'>Created by Joshua Perry</small>
+    </div>
+  );
+}
+
+export default Footer;
