4a365e2c22ec57fc31c190bf3279bee0f91a4a2c
Joshua Perry <joshuacperry95@gmail.com>
2021-05-10 17:59:49 -0500

    Improve logic for determining date from calendar

--- src/components/APODSubSection.js
+++ src/components/APODSubSection.js
@@ -34,13 +34,12 @@ export default class APODSubSection extends React.Component {
 		if (month.length < 2) {
 			month = "0" + month;
 		}
-
-		var day = this.props.date.substring(this.props.date.indexOf('/') + 1, this.props.date.indexOf('/', 2))
+		var day = this.props.date.substring(this.props.date.indexOf('/') + 1, this.props.date.indexOf('/', 3))
 		if (day.length < 2) {
 			day = "0" + day;
 		}
 
-		var year = this.props.date.substring(this.props.date.indexOf('/', 2) + 1);
+		var year = this.props.date.substring(this.props.date.indexOf('/', 3) + 1);
 
 		var url = "https://api.nasa.gov/planetary/apod?api_key=kaOBcWpith5vDxTLRNsiw6VWOXEt5nVmBcupwgov&date="
 		+ year + "-" + month + "-" + day;
