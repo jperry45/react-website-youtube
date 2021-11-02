f572c0d25bf28bc2af04ab463ffa5d3eabe51d6b
Joshua Perry <joshuacperry95@gmail.com>
2021-11-02 17:38:00 -0500

    Updates

---
+++ precache-manifest.d3fb9eb3f99a250e0f8042e90717f0ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e88176b4fa234178b1aee9fa749c3f",
+    "url": "/react-website-youtube/index.html"
+  },
+  {
+    "revision": "219f59b697b2e0568b60",
+    "url": "/react-website-youtube/static/css/main.d148b12f.chunk.css"
+  },
+  {
+    "revision": "a5ece19d3f4a4993c833",
+    "url": "/react-website-youtube/static/js/2.7941eed2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-website-youtube/static/js/2.7941eed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219f59b697b2e0568b60",
+    "url": "/react-website-youtube/static/js/main.3afb77c7.chunk.js"
+  },
+  {
+    "revision": "0667468f4ebc1428a203",
+    "url": "/react-website-youtube/static/js/runtime-main.48f3b6dc.js"
+  },
+  {
+    "revision": "27a705eccfc790026e776eb6f2e9f7b4",
+    "url": "/react-website-youtube/static/media/frontpage.27a705ec.jpg"
+  },
+  {
+    "revision": "27a705eccfc790026e776eb6f2e9f7b4",
+    "url": "/react-website-youtube/static/media/search.27a705ec.jpg"
+  }
+]);
